5502d3bcc5a6e645f7d33aba37ee0033a21b9b49
DHANOOP-P <dhanoopp6@gmail.com>
2026-08-12 17:32:21 +0530

    trial

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,16 +5,18 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
@@ -32,4 +34,8 @@ if (fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
   connectDB();
   console.log("The server listen at port:", PORT);
+
+  if(process.env.NODE_ENV === "production"){
+    job.start();
+  }
 });
