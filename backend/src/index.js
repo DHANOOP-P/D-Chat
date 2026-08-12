74a5ebd8137a078b1963c413c7ab8819adf9a7b5
DHANOOP-P <dhanoopp6@gmail.com>
2026-08-12 13:08:55 +0530

    the trial for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express"
 import "dotenv/config"
+import User from "./models/user.models.js"
+import { connectDB } from "./lib/db.js"
 
 
 
 const app=express()
 const PORT=process.env.PORT
-
+connectDB();
 
 app.listen(3000,()=>{
-    console.log("the server listen at port 3000",PORT)
+    console.log("The server listen at port:",PORT)
 })   
